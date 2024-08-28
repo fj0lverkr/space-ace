ece73c242ca5684d3eb2f82198b37c6a9d5215c5
Nils Nahooy <nils.nahooy@live.com>
2024-08-28 20:06:19 +0200

    basic shield implementation

--- scenes/shield/shield.gd
+++ scenes/shield/shield.gd
@@ -3,6 +3,8 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 @onready var collission_shape: CollisionShape2D = $CollisionShape2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sound: AudioStreamPlayer2D = $Sound
 
 @export var max_hits: int = 5
 
@@ -12,7 +14,8 @@ var _hits_left: int
 
 func _ready() -> void:
 	_hits_left = max_hits
-	_deactivate()
+	#_deactivate(false)
+	activate()
 
 
 func activate() -> void:
@@ -21,24 +24,32 @@ func activate() -> void:
 
 	_is_active = true
 	_hits_left = max_hits
-	collission_shape.disabled = false
+	collission_shape.call_deferred("set_disabled", false)
 	show()
 	timer.start()
 
 
-func _deactivate() -> void:
-	if not timer.is_stopped():
-		timer.stop()
-
+func _deactivate(do_flicker: bool = true) -> void:
+	timer.stop()
 	_is_active = false
-	collission_shape.disabled = true
-	hide()
+	collission_shape.call_deferred("set_disabled", true)
+	if do_flicker:
+		animation_player.play("flicker")
+	else:
+		hide()
 
 
 func _on_timer_timeout() -> void:
 	_deactivate()
 
+
 func _on_area_entered(_area: Area2D) -> void:
 	_hits_left -= 1
-	if _hits_left >= 0:
+	animation_player.play("hit")
+	if _hits_left == 0:
 		_deactivate()
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "flicker":
+		hide()
