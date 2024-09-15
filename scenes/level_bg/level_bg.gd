acc0094eb378995eb4fe10d1324e0d018512eb51
Nils Nahooy <nils.nahooy@live.com>
2024-09-15 15:31:31 +0200

    fix paralax layers

--- scenes/level_bg/level_bg.gd
+++ scenes/level_bg/level_bg.gd
@@ -4,4 +4,6 @@ extends ParallaxBackground
 
 
 func _process(delta: float) -> void:
-	scroll_offset.y += delta * scroll_speed
+	for l: Node in get_children():
+		if l is ParallaxLayer:
+			l.motion_offset.y += delta * scroll_speed * randf_range(0.2, 0.5)
