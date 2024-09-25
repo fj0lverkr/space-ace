64703909002eecc7936be103c51f992eacaacf9e
Nils Nahooy <nils.nahooy@live.com>
2024-09-25 19:49:48 +0200

    Fix bombs

--- scenes/bullets/bomb/enemy_bomb.gd
+++ scenes/bullets/bomb/enemy_bomb.gd
@@ -1,2 +1,10 @@
 class_name EnemyBombBullet
 extends BaseBullet
+
+@onready
+var _sfx: AudioStreamPlayer2D = $Sfx
+
+
+func _ready() -> void:
+    super._ready()
+    SoundManager.play_by_type(_sfx, SoundManager.SoundType.LASER)
