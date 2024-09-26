ff081e5c60f5b4b472503e97df5ea9acd5345c53
Nils Nahooy <nils.nahooy@live.com>
2024-09-26 18:18:10 +0200

    add sprite an basic logic

--- scenes/enemies/saucer.gd
+++ scenes/enemies/saucer.gd
@@ -1,12 +1,47 @@
 class_name Saucer
 extends BaseEnemy
 
+@onready
+var _marker_gun: Marker2D = $MarkerGun
 
-# Called when the node enters the scene tree for the first time.
+const HEALTH: Dictionary = {
+	SubType.YELLOW: 100,
+	SubType.BLUE: 100,
+	SubType.RED: 100,
+}
+
+const SPEED: Dictionary = {
+	SubType.YELLOW: 100,
+	SubType.BLUE: 100,
+	SubType.RED: 100
+}
+
+const DROPRATES: Dictionary = {
+	SubType.YELLOW: 15,
+	SubType.BLUE: 15,
+	SubType.RED: 15,
+}
+
+const POINTS: Dictionary = {
+	SubType.YELLOW: 100,
+	SubType.BLUE: 100,
+	SubType.RED: 100,
+}
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	super._ready()
+	sprite.play(_subtype)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	super._process(delta)
+	_shoot(_marker_gun.global_position)
+
+
+func setup(s: SubType) -> void:
+	_health_data = HEALTH
+	_speed_data = SPEED
+	_drop_rate_data = DROPRATES
+	_points_data = POINTS
+	super.setup(s)
