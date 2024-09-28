63ee22720bfda69522c4f3d89658acdff969f401
Nils Nahooy <nils.nahooy@live.com>
2024-09-28 13:37:13 +0200

    base for saucer enemy done

--- scenes/enemies/saucer.gd
+++ scenes/enemies/saucer.gd
@@ -1,11 +1,20 @@
+class_name Saucer
 extends PathFollow2D
+
+const SPEED: float = 20.0
+
+@onready
+var _health_bar: HealthBar = $HealthBar
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_health_bar.setup(250, 250)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	progress += delta * SPEED
+
+	if progress_ratio > 0.99:
+		queue_free()
