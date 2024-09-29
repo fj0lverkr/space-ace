13b23ac331db24703f0a733fa5bd2422dd95c0a3
Nils Nahooy <nils.nahooy@live.com>
2024-09-29 11:21:09 +0200

    Saucer can die

--- scenes/enemies/saucer.gd
+++ scenes/enemies/saucer.gd
@@ -1,11 +1,17 @@
 class_name Saucer
 extends PathFollow2D
 
-const SPEED: float = 20.0
 const WAIT_TIME: float = 7.0
 const WAIT_VAR: float = 1.0
 const PB: String = "parameters/playback"
 const SHOOT: String = "shoot"
+const DIE: String = "die"
+const BOOM_TIMEOUT: float = 0.2
+
+@export
+var _health: int = 1500
+@export
+var _points: int = 150
 
 @onready
 var _health_bar: HealthBar = $HealthBar
@@ -21,13 +27,16 @@ var _hitbox: CollidableObject = $Hitbox
 var _sprite: Sprite2D = $Hitbox/Sprite2D
 @onready
 var _sound: AudioStreamPlayer2D = $Sound
+@onready
+var _booms: Node2D = $Hitbox/Booms
 
+var _speed: float = 20.0
 var _is_shooting: bool = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_health_bar.setup(250, 250)
+	_health_bar.setup(_health, _health)
 	GameUtils.set_and_start_timer(_shoot_timer, WAIT_TIME, WAIT_TIME + WAIT_VAR)
 
 
@@ -36,7 +45,7 @@ func _process(delta: float) -> void:
 	if _is_shooting:
 		return
 
-	progress += delta * SPEED
+	progress += delta * _speed
 
 	if progress_ratio > 0.99:
 		queue_free()
@@ -57,3 +66,22 @@ func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
 	if anim_name == SHOOT:
 		_is_shooting = false
 		GameUtils.set_and_start_timer(_shoot_timer, WAIT_TIME, WAIT_VAR)
+
+
+func _on_hitbox_on_area_entered(area: Area2D) -> void:
+	var damage: int
+	if area is CollidableObject:
+		damage = area.get_damage()
+
+	_health_bar.update_value(-damage)
+
+
+func _on_health_bar_on_died() -> void:
+	_health_bar.hide()
+	_hitbox._disable_collision()
+	_speed = 0.0
+	SignalBus.on_score_points.emit(_points)
+	_state_machine.travel(DIE)
+	for m: Marker2D in _booms.get_children():
+		SignalBus.on_explode.emit(Explosion.Type.BOOM, m.global_position, 2.0)
+		await get_tree().create_timer(BOOM_TIMEOUT).timeout
